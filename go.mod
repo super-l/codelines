49f1ab881c28f703f2b30aae052d4784164f3b77
superl@nepenthes.cn <echo"superl1994>
2021-10-11 17:55:44 +0800

    logo

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/super-l/codelines
 
 go 1.14
+
+require github.com/gookit/color v1.4.2
--- main.go
+++ main.go
@@ -9,10 +9,28 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gookit/color"
 	"github.com/super-l/codelines/core"
 )
 
+func showBanner() {
+	banner := `
+codelines v1.0
+Created by superl[忘忧草安全]             QQ: 86717375
+
+               _      _ _ _                 
+              | |    ( ) (_)                
+  ___ ___   __| | ___|/| |_ _ __   ___  ___ 
+ / __/ _ \ / _' |/ _ \ | | | '_ \ / _ \/ __|
+| (_| (_) | (_| |  __/ | | | | | |  __/\__ \
+\___\___/ \__,_|\___| |_|_|_| |_|\___||___/
+
+`
+	color.Green.Println(banner)
+}
+
 func main(){
+	showBanner()
 	config, err := core.LoadConfigData()
 	if err != nil {
 		fmt.Println(err.Error())
